3a661aa3a0ac0a0909167c90e2fd877ba98f6d8d
Matheus Rodrigues <matheusrodriguesh1@gmail.com>
2022-02-15 00:26:53 -0300

    correção frases e botoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,32 @@
+function mudaModo() {
+    mudaClasses()
+    mudaTextos()
+}
+
+function mudaClasses(){
+    h1.classList.toggle(darkModeClasse)
+    button.classList.toggle(darkModeClasse)
+    footer.classList.toggle(darkModeClasse)
+    corpo.classList.toggle(darkModeClasse)
+}
+
+function mudaTextos(){
+    const lightMode = "Light Mode"
+    const darkMode = "Dark Mode"
+
+    if(corpo.classList.contains(darkModeClasse) ){
+        button.innerHTML = lightMode
+        h1.innerHTML = darkMode + " ON"
+        return
+    }
+    button.innerHTML = darkMode
+    h1.innerHTML = lightMode + " ON"
+}
+
+const darkModeClasse = 'dark-mode'
 const h1 = document.querySelectorAll('#page-title')[0]
 const button = document.querySelectorAll('#mode-selector')[0]
 const footer = document.querySelector('footer')
 const corpo = document.querySelector('body')
 
-button.addEventListener("click", function() {
-    // alert('oi')
-    h1.classList.toggle('dark-mode')
-    button.classList.toggle('dark-mode')
-    footer.classList.toggle('dark-mode')
-    corpo.classList.toggle('dark-mode')
-})
-
+button.addEventListener("click", mudaModo)
